e67c3ad37844fefb62f018f23b25e779e58d08e2
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-07-28 07:10:44 +0300

    number formater

---
+++ CodeWars/6-kyu/Number-Format.js
@@ -0,0 +1,11 @@
+function formatNumber(n) {
+    let str = Math.abs(n).toString().split('').reverse();
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        if (i % 3 === 0 && i !== 0) arr.push(',');
+        arr.push(str[i]);
+    }
+    return n / Math.abs(n) === -1 ? '-' + arr.reverse().join('') : arr.reverse().join('');
+}
+
+console.log(formatNumber(-420902 ));
