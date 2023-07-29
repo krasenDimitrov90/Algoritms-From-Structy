0cd836dcf1a3b326707156a1bf8d0b5d86969fc3
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-07-29 08:33:29 +0300

    number of invertions in array

---
+++ CodeWars/6-kyu/Calculate number of inversions in array.js
@@ -0,0 +1,18 @@
+function countInversions(nums) {
+    let count = 0;
+    let isSwap = true;
+    while (isSwap) {
+        isSwap = false;
+        for (let i = 0, l = nums.length; i < l; i++) {
+            if (nums[i + 1] < nums[i]) {
+                [nums[i], nums[ i + 1]] = [nums[i + 1], nums[i]];
+                isSwap = true;
+                count++;
+            }
+        }
+    }
+   
+    return count;
+}
+
+console.log(countInversions([6, 5, 4, 3, 2, 1]));
