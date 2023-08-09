eb67f50dd5d1d4b6bb2243a67788ab3e73028e3b
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-08-09 20:33:00 +0300

    perimeter of squares

---
+++ CodeWars/5-kyu/Perimeter of squares in a rectangle.js
@@ -0,0 +1,20 @@
+function perimeter(n) {
+    const fib = (n, start = true, memo = { sum: 1 }) => {
+        if (n === 0) return 1;
+        if (n < 0) return 0;
+        if (n in memo) return memo[n];
+
+        memo[n] = fib(n - 1, false, memo) + fib(n - 2, false, memo);
+        memo.sum += memo[n];
+        return start ? memo.sum : memo[n];
+    };
+    // console.log(fib(n))
+    return 4 * fib(n);
+}
+
+
+console.log(perimeter(0)); // 4)
+console.log(perimeter(5)); // 80)
+console.log(perimeter(7)); // 216)
+console.log(perimeter(20)); // 114624)
+console.log(perimeter(30)); // 14098308)
